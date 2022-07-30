f14b4053b47ea8e768216abd35b44f30f0b7ccea
prasanthbangs2016 <prasanthbangs@gmail.com>
2022-07-30 16:28:49 +0530

    added sample file

--- Roboshop/component/frontend.sh
+++ Roboshop/component/frontend.sh
@@ -1,8 +1,11 @@
 #bin/bash
 #gold colour
 echo -e "\e[1m""----------------------------------"
-echo -e Installing Frontend
+echo -e UnInstalling Frontend
 echo -e "----------------------------------\e[0m"
 
+yum remove nginx -y &>>/tmp/roboshop.log
 
-yum install nginx -y &>>/tmp/roboshop.log
+echo -e "\e[1m""----------------------------------"
+echo -e "\e[1;32mSUCCESS"
+echo -e "----------------------------------\e[0m"
