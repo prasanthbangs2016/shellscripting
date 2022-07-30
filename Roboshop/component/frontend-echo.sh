a00e1b04ccee82438beb43cf5a986ab78de6f29a
prasanthbangs2016 <prasanthbangs@gmail.com>
2022-07-30 17:35:18 +0530

    added file

---
+++ Roboshop/component/frontend-echo.sh
@@ -0,0 +1,19 @@
+#bin/bash
+#gold colour
+
+#maven approach
+echo -e "\e[1m""----------------------------------"
+echo -e UnInstalling Frontend
+echo -e "----------------------------------\e[0m"
+
+#yum install nginx -y &>>/tmp/roboshop.log
+
+echo -e "\e[1m""----------------------------------"
+echo -e "\e[1;32mSUCCESS\e[0m"
+echo -e "----------------------------------\e[0m"
+
+
+#docker approach
+echo -n "installing nginx...."
+#yum install nginx -y &>>/tmp/roboshop.log
+echo -e "\e[32mDONE\e[0m"
--- Roboshop/component/frontend.sh
+++ Roboshop/component/frontend.sh
@@ -1,19 +1,9 @@
 #bin/bash
 #gold colour
 
-#maven approach
-echo -e "\e[1m""----------------------------------"
-echo -e UnInstalling Frontend
-echo -e "----------------------------------\e[0m"
-
-#yum install nginx -y &>>/tmp/roboshop.log
-
-echo -e "\e[1m""----------------------------------"
-echo -e "\e[1;32mSUCCESS\e[0m"
-echo -e "----------------------------------\e[0m"
-
+source component/common.sh
 
 #docker approach
-echo -n "installing nginx...."
+HEAD "Installing Nginx"
 #yum install nginx -y &>>/tmp/roboshop.log
 echo -e "\e[32mDONE\e[0m"
