2c4bbe74cd057a5358687a006cabc7fa06f88d9b
prasanthbangs2016 <prasanthbangs@gmail.com>
2022-07-30 23:30:42 +0530

    added file

--- Roboshop/component/frontend.sh
+++ Roboshop/component/frontend.sh
@@ -10,15 +10,7 @@ HEAD "Installing Nginx\t\t\t"
 yum install nginx -y &>>/tmp/roboshop.log
 #script will fail if run as below
 #yum install Nginx -y &>>/tmp/roboshop.log
-
 STAT $?
-
-<<##
-HEAD "start nginx\t\t\t\t"
-systemctl start nginx &>>/tmp/roboshop.log
-systemctl enable nginx &>>/tmp/roboshop.log
-STAT $?
-##
 
 HEAD "Downloading frontend application code\t"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>/tmp/roboshop.log
