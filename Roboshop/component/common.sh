af923299ef038a4934c6a74c892613d0e402af51
prasanthbangs2016 <prasanthbangs@gmail.com>
2022-07-30 18:04:33 +0530

    added file

--- Roboshop/component/common.sh
+++ Roboshop/component/common.sh
@@ -5,7 +5,7 @@ HEAD() {
 #exit status
 STAT() {
     if [ $1 -eq 0]; then
-      echo -e "\e[1;32m done\e[0m"
+        echo -e "\e[1;32m done\e[0m"
     else
         echo -e "\e[1;31m fail\e[0m"
         echo -e "\t check the log file for more details... Log file: /tmp/roboshop.log\e[0m"
