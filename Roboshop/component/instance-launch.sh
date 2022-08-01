36dc664756c43586a1a9e1ee73b8a7a5b70de581
prasanthbangs2016 <prasanthbangs@gmail.com>
2022-08-01 15:11:53 +0530

    added script

--- Roboshop/component/instance-launch.sh
+++ Roboshop/component/instance-launch.sh
@@ -13,13 +13,14 @@ LID=lt-0b86176ba30da7a45
 version=1
 
 #validate instance state and if it is already exist and running it wont create instance,it will create instance only when it is not available.
+
 INSTANCE_STATE=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].State.Name | xargs -n1)
-if [ "$(INSTANCE_STATE)" = "running"]; then
-  echo "Frontend Instance is already running/stopped and exist hence cannot launch"
+if [ "$(INSTANCE_STATE)" = "running" ]; then
+  echo "Frontend Instance is already running or stopped and exist hence cannot launch"
   exit 0
 fi
 
-if [ "$(INSTANCE_STATE)" = "stopped"]; then
+if [ "$(INSTANCE_STATE)" = "stopped" ]; then
   echo "Frontend Instance is already running/stopped and exist hence cannot launch"
   exit 0
 fi
